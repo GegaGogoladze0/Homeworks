89aa5b9791eacc1ef6505d62d94496bc9eec433e
GegaGogoladze <gogoladzegega4@gmail.com>
2024-03-13 18:35:40 +0400

    commit

--- day 6/homework.py
+++ day 6/homework.py
@@ -5,3 +5,10 @@ fro=input("enter whrere are you from: ")
 proffesion=input("what is your proffesion: ")
 hobby=input("what is your hobby: ")
 print(name + " " + surname + " " + age + " " + fro + " " + proffesion + " " + hobby)
+
+
+
+num1=int(input("enter your age: "))
+num2=int(input("enter your age: "))
+num3=int(input("enter your age: "))
+print(num1 + 25 , num2 + 25, num3 + 25)

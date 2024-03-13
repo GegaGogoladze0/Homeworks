f2fc65e8b47309035767e67598b36b740eecb373
GegaGogoladze <gogoladzegega4@gmail.com>
2024-03-13 16:09:46 +0400

    commit

---
+++ day 6/homework.py
@@ -0,0 +1,7 @@
+name=input ("enter your name:")
+surname=input("enter your surname: ")
+age=input("enter your age: ")
+fro=input("enter whrere are you from: ")
+proffesion=input("what is your proffesion: ")
+hobby=input("what is your hobby: ")
+print(name + " " + surname + " " + age + " " + fro + " " + proffesion + " " + hobby)
